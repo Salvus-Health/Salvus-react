6f42359359487d294740dcda62de034ebb46d296
jordiahl <jordiahl@ufl.edu>
2019-01-15 18:36:39 -0500

    carousel somewhat working, needs to improve the visuals

--- src/landing-page/info-section/InfoSection.tsx
+++ src/landing-page/info-section/InfoSection.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {
     Carousel,
-    CarouselControl,
+    // CarouselControl,
     CarouselIndicators,
     CarouselItem
 } from 'reactstrap';
@@ -15,10 +15,10 @@ interface IInfoState {
 class InfoSection extends React.Component <any, IInfoState> {
 
     private items: LandingPageInfo [] = [
-        new LandingPageInfo("/assets/Holding-Hands-Doctor.svg", "this is my info"),
+        new LandingPageInfo("/assets/images/landing/stories-ambulance.png", "this is my info"),
         new LandingPageInfo("/assets/Holding-Hands-Doctor.svg", "this is my changed info"),
     ];
-    private animating = true;
+    private animating = false;
 
     constructor(props: any) {
         super(props);
@@ -41,19 +41,25 @@ class InfoSection extends React.Component <any, IInfoState> {
     }
 
     public next() {
-        if (this.animating) { return; }
+        if (this.animating) {
+            return;
+        }
         const nextIndex = this.state.activeIndex === this.items.length - 1 ? 0 : this.state.activeIndex + 1;
         this.setState({activeIndex: nextIndex});
     }
 
     public previous() {
-        if (this.animating) { return; }
+        if (this.animating) {
+            return;
+        }
         const nextIndex = this.state.activeIndex === 0 ? this.items.length - 1 : this.state.activeIndex - 1;
         this.setState({activeIndex: nextIndex});
     }
 
     public goToIndex(newIndex: number) {
-        if (this.animating) { return; }
+        if (this.animating) {
+            return;
+        }
         this.setState({activeIndex: newIndex});
     }
 
@@ -68,27 +74,41 @@ class InfoSection extends React.Component <any, IInfoState> {
                     onExited={this.onExited}
                     key={item.img}
                 >
-                    <div className="col-md-12">
-                        <div className="col-md-6">   
-                            <img className="col-md-6" src={item.img} alt={"Alt text"}/></div>
+                    <div className="col-md-12 row" style = {{marginBottom:"80px"}}>
+                        <div className="col-md-6">
+                            <img className="col-md-6" src={item.img} alt={"Alt text"}/>
+                        </div>
 
-                        <p className="col-md-6">this is the info that is needed </p></div>
+                        <div className="col-md-6 info-container">
+                            <p className="title-s col-md-12">Emergency Care insurance</p>
+                            <p className="description-s col-md-12">
+                                In the realm of DPC, one has many questions regarding a big life
+                                event...
+                            </p>
+
+                        </div>
+
+                    </div>
                     {/*<CarouselCaption captionText={item.info} captionHeader={item.info}/>*/}
                 </CarouselItem>
             );
         });
 
-        return <Carousel
-            activeIndex={activeIndex}
-            next={this.next}
-            previous={this.previous}
-            className = "col-md-12"
-        >
-            <CarouselIndicators items={this.items} activeIndex={activeIndex} onClickHandler={this.goToIndex}/>
-            {slides}
-            <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous}/>
-            <CarouselControl direction="next" directionText="Next" onClickHandler={this.next}/>
-        </Carousel>;
+        return (
+
+            <div className="col-md-12 info-s">
+                <Carousel
+                    activeIndex={activeIndex}
+                    next={this.next}
+                    previous={this.previous}
+                    className="col-md-12"
+                >
+                    {slides}
+                    <CarouselIndicators items={this.items} activeIndex={activeIndex} onClickHandler={this.goToIndex}/>
+                    {/*<CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous}/>*/}
+                    {/*<CarouselControl direction="next" directionText="Next" onClickHandler={this.next}/>*/}
+                </Carousel>
+            </div>);
     }
 
 }
